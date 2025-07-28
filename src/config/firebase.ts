d4fde3225f26e222c6788d91cd0f57c56cfc5f43
Julian D <judadi1994@gmail.com>
2025-07-28 10:12:22 +0200

    "Add function to delete anonymous Firebase users for improved user management"

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,5 +1,6 @@
 import admin from "firebase-admin";
 import serviceAccount from "../../serviceAccountKey.json";
+import {getAuth} from "firebase-admin/auth";
 
 
 if (!admin.apps.length) {
@@ -10,6 +11,24 @@ if (!admin.apps.length) {
 } else {
     console.warn('⚠️ Firebase Admin was already initialized or failed to initialize')
 }
-
-
 export default admin
+
+const deleteAnonymousUsers = async () => {
+    getAuth().listUsers(20)
+        .then(function (listUsersResult) {
+            listUsersResult.users.forEach(function (userRecord) {
+                if (userRecord.providerData.length === 0) { //this user is anonymous
+                    console.log(userRecord)
+                    getAuth().deleteUser(userRecord.uid).then(function () {
+                        console.log("Successfully deleted user");
+                    })
+                        .catch(function (error) {
+                            console.log("Error deleting user:", error);
+                        });
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log('Error listing users:', error);
+        });
+}
